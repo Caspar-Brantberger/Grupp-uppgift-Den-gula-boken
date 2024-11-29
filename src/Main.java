37b0e746d9965a896f42e623497b4a9393a2b8be
JonathanJirefalk <3tomu24.jirejo@folkuniversitetet.nu>
2024-11-29 10:50:39 +0100

    Login Function

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,9 @@ public class Main {
 
     public static boolean runProgram = true;
 
+    public static boolean isAdmin = true;
+    public static boolean isLoggedIn = false;
+
     public static void main(String[] args) {
 
 
@@ -17,17 +20,89 @@ public class Main {
         createContact("Sven", "Eriksson", "07134578");
 
         while(runProgram) {
-            userInput();
+
+            while(!isLoggedIn) {
+
+                loginPrompt();
+            }
+
+            if(isAdmin) {
+
+                adminInput();
+            } else {
+
+                guestInput();
+            }
         }
 
     }
 
-    public static void userInput(){
+    public static void loginPrompt(){
+        System.out.println("Hello, how would you like to login?");
+        System.out.println("1. Guest");
+        System.out.println("2. Admin");
+
+        switch (scanner.nextLine()){
+            case "1":
+                isLoggedIn = true;
+                isAdmin = false;
+                break;
+            case "2":
+                adminLogin();
+                break;
+            default:
+                System.out.println("Invalid Input!");
+        }
+
+    }
+
+    public static void adminLogin(){
+
+        String[] userName = {"Jonathan", "Caspar", "Angelica", "Ali", "William"};
+        String[] password = {"jo", "ca", "an", "al", "wi"};
+
+        int indexOfName = 0;
+        boolean validUsername = false;
+
+        System.out.println("Please Insert Username:");
+        String username = scanner.nextLine();
+
+        for (int i = 0; i < userName.length; i++) {
+
+            if(username.equals(userName[i])) {
+                indexOfName = i;
+                validUsername = true;
+            }
+        }
+
+        if(!validUsername) {
+
+            System.out.println("Invalid username, please try again!");
+            adminLogin();
+        }
+        else{
+            System.out.println("User: " + userName[indexOfName]);
+            System.out.println("Please insert password: ");
+
+            while(!scanner.nextLine().equals(password[indexOfName])) {
+
+                System.out.println("Password incorrect, please try again!");
+            }
+
+            System.out.println("Successfully logged in as " + userName[indexOfName] + "!");
+            isAdmin = true;
+            isLoggedIn = true;
+
+        }
+    }
+
+    public static void adminInput(){
         System.out.println("What action would you like to do?");
         System.out.println("1. Display phonebook");
         System.out.println("2. Remove Contact");
         System.out.println("3. Create New Contact");
-        System.out.println("4. Exit Program");
+        System.out.println("4. Search Contact");
+        System.out.println("5. Logout");
 
         switch(scanner.nextLine()){
             case "1":
@@ -40,7 +115,33 @@ public class Main {
                 newContact();
                 break;
             case "4":
-                runProgram = false;
+                System.out.println("Sökfunktion ej gjord än!");
+                break;
+            case "5":
+                isAdmin = false;
+                isLoggedIn = false;
+                break;
+            default:
+                System.out.println("Not a valid input");
+        }
+    }
+
+    public static void guestInput(){
+        System.out.println("What action would you like to do?");
+        System.out.println("1. Display phonebook");
+        System.out.println("2. Search Contact");
+        System.out.println("3. Logout");
+
+        switch(scanner.nextLine()){
+            case "1":
+                displayContacts();
+                break;
+            case "2":
+                System.out.println("Sökfunktion ej gjord än!");
+                break;
+            case "3":
+                isLoggedIn = false;
+                isAdmin = false;
                 break;
             default:
                 System.out.println("Not a valid input");
